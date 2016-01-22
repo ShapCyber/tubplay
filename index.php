6bafcfa8920475783bfe0600e419853300aa9866
ShapCyber <adego247@gmail.com>
2016-01-22 09:53:07 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html, charset=UTF-8" />
+<meta name="author" content="Simlofi" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+<meta name="twitter:card" content="app">
+<meta name="twitter:description" content="Something to be play (TubPlay). The smart application let you sell your Digital Media such as podcast, audio and video. It has an Admin panel where you can add your files">
+<meta name="twitter:app:country" content="GB">
+<meta name="twitter:app:name:googleplay" content="TubPlay">
+<meta name="twitter:app:id:googleplay" content="com.phonegap.tubplay">
+<meta name="twitter:app:url:googleplay" content="https://play.google.com/store/apps/details?id=com.phonegap.tubplay">
+<meta name="twitter:site" content="@tubentertain">
+<meta name="twitter:creator" content="@tubentertain">
+<title>TubPlay | Sell Your Digital Media</title>
+<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+<link href="css/tubplay.css" rel="stylesheet" type="text/css" />
+</head>
+<body> 
+<div id="TubPlayFrame" >  
+<!--TubPlay Design And Develop By Adegoroye Owolabi-->
+<!--MAINFrame Start Here -->
+<div id="TubPlayplayer" class="TubPlayplayer"> 
+<div  id=toTubPlay>
+Select by &nbsp;  <a id="moregenres">Genres</a>
+<input id=TubPlay placeholder="Type Here n Search" value=""/>
+<button id=submitTubPlay>th</button>
+</div> 
+<div id=genresHolder>
+More Genres <span class=close>x</span>
+<hr/>
+<div id=genresList>
+<ul id=moreGenresHolder>
+</ul>
+</div>
+</div>
+<div id="marquee" class="marquee">TubPlay <span id="mqr">now Playing...  <span id="tyt">Get A Copy Of This Player </span> Customised For You  <span class="name">..More At www.tubentertain.com<span></span></div>
+<div id="playerHolder" class="player">
+<div class="loadering"><span class="spining"><i class="fa fa-spinner fa-pulse"></i></span></div>
+<div id="Tubshare" class="nChilds noshow" >
+<a> <i class="fa fa-share-alt"></i></a>
+<a id="sTwitter" class="TubPlaySocial" title="share selected song on twitter"  target="_blank">
+<i class="fa fa-twitter"></i></a>
+<a id="sGooglePlus" class="TubPlaySocial" title="share selected song on google" target="_blank">
+<i class="fa fa-google-plus"></i></a>
+<a id="sFaceBook" class="TubPlaySocial" title="share selected song on facebook" target="_blank">
+<i class="fa fa-facebook-official"></i></a></div> 
+
+  <div id="wrapUp">
+<video id="tubVideo" src="extraFile/oceans-clip.mp4"  autoplay ></video>
+  <audio id="tubAudio" autoplay  src="" ></audio>
+  </div> 
+  
+    <img id="biuyNow" src="css/images/13bg.jpg" width="100"  alt="biuyNow">	
+	
+    <div id="controls" class="controls noshow">
+
+	  <span class="duraTion">00:00:00</span>
+	<span class="timeNow">00:00:00</span>
+
+	
+	<div class="railHolder"><span class="railInner"></span></div>    
+	  <span class="rew" title="previous"><i class="fa fa-step-backward"></i></span>
+	  <span class="play" title="play"><i class="fa fa-play"></i></span>
+      <span class="pause" title="pause"><i class="fa fa-pause"></i></span>
+      <span class="fwd" title="next"><i class="fa fa-step-forward"></i></span>
+	 <span class="exitFull"	title="exit fullscreen" ><i class="fa fa-arrows-alt"></i></span> 
+	<span class="full"	title="fullscreen" ><i class="fa fa-arrows-alt"></i></span>
+	<div id="volumeHolder">	
+	<span class="volumeHandle"></span>
+	<span class="volumeRaylay"></span>
+	</div>	  
+	  <span class="volumeUp"	title="Mute" ><i class="fa fa-volume-up"></i></span> 
+	   <span class="volumeDown"	title="UnMute" ><i class="fa fa-volume-up"></i></span> 
+	  
+	  </div>   
+	 
+  </div>
+   
+    <div id="Listhoder" >   
+    <ul class="playlist" id="playlist"><li><span id="waiter"><i class="fa fa-cog fa-spin"></i> Getting  data please hold on.....</span></li></ul>	
+</div>
+<div id="Tubfooter" class="" >
+<a id="aboutTubPlay"  title="All about TubPlay" target="_blank"><i class="fa fa-info-circle"></i> Info...</a>
+<a id="hideInfo"  title="Hide Info" target="_blank"><i class="fa fa-info-circle"></i> Hide</a>
+<a href="http://tubentertain.com"  target="_blank">Powered by TubEntertain&trade;</a></div> 
+</div>
+
+<div id="TubPlayInfo" >
+<div id="TubPlaypageHolder" >
+<div class="TubPlaypage" >
+<p>
+<img  src="tubplay.jpg" alt="TubPlay" width="100" />
+Something to be play (TubPlay). 
+<strong>Tub<span class="ewa">play</span></strong>   is another beautiful  application developed and designed by  <strong>Tub<span class="ododo" >E</span><span class="ewa" >ntertain</span></strong> developer. The smart application let you sell your Digital Media such as podcast, audio and video.</p>
+<p>
+<img id="TubPlay" src="developios.jpg" alt="ios sampl" width="100"/>
+If you are having a live radio station, or Internet Tv , the application let you embed the live stream. User can search, preview, pay and download your digital media. If you are into iTunes affiliation program this is a perfect application for you. The application run on mobile and web, eliminate advert blockage, serve site visitors with your advert, and make more money.</p>
+<p>
+Now that you got the idea try to search for your favorite song, video either by Genres or Artist name and get their latest digital media.</p>
+<p>For example you can type in Artist name such as  Micheal Jackson, Psquare , Olly or by Genres e.g. Classic music, Inspirational music, Gospel music, Hip Hop,  Rap, or  by song / movie title. For sermons and lectures type in Preacher or Lecturer name and so on or select the <span class="ewa">List Genres </span> from drop-down button at the top and press on Genre of your choice.<br />
+ Enjoy !</p>
+<p>
+<strong class="jwn">Warning !</strong> <em>Some of media might not play if your browser does not support type of file format, such as M4V.</em></p>
+</div>
+</div>
+</div>
+</div>
+<script src="js/jscroller.js"></script>	
+<script src="js/tubplay.js"></script>	
+</body>
+</html>
